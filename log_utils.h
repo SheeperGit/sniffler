58094fe8c0702dafd30bf6b5f532f03b09fe34ed
SheeperGit <sean1900.s@gmail.com>
2024-12-08 14:59:14 -0500

    prettify log_utils.h

--- log_utils.h
+++ log_utils.h
@@ -1,4 +1,9 @@
-void logEthHdr(unsigned char *, int);
-void logIPHdr(unsigned char *, int);
-void dumpPkt(unsigned char *, int);
-void logPktData(unsigned char *, int);
+#ifndef _LOG_UTILS_H
+#define _LOG_UTILS_H
+
+void logEthHdr(unsigned char *buf, int size);
+void logIPHdr(unsigned char *buf, int size);
+void dumpPkt(unsigned char *buf, int size);
+void logPktData(unsigned char *data, int size);
+
+#endif
